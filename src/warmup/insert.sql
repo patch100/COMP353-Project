b117031432628f0d1613eca7155eba3351440b75
Gabriel Hardy <gabrielhardy@protonmail.ch>
2016-02-10 19:44:43 -0500

    Modified schemas to support bigger addresses; Started insertion script.

---
+++ src/warmup/insert.sql
@@ -0,0 +1,35 @@
+use comp353_warmup;
+
+INSERT INTO Customer
+VALUES (1, "Usha Trevizo", "839 Meadow Street Aberdeen, SD 57401", "613-555-0110"),
+(2, "Sharika Comeau", "661 Amherst Street Clarksville, TN 37040", "613-555-0166"),
+(3, "Tesha Kearney", "481 Grant Street Lapeer, MI 48446", "613-555-0132"),
+(4, "Gail Dabbs", "208 7th Street Kent, OH 44240", "613-555-0178"),
+(5, "Amira Beers", "419 Washington Street Ridgefield, CT 06877", "613-555-0103"),
+(6, "Shyla Pinedo", "771 Canterbury Road Glasgow, KY 42141", "613-555-0117"),
+(7, "Wonda Gee", "621 Mulberry Street Windsor Mill, MD 21244", "613-555-0175"),
+(8, "Francesco Juhasz", "463 Maple Lane Port Richey, FL 34668", "613-555-0176"),
+(9, "Gertha Aubrey", "23 Wall Street Littleton, CO 80123", "613-555-0148"),
+(10, "Jefferey Mcgehee", "313 Prospect Street Warren, MI 48089", "613-555-0160"),
+(11, "Sherryl Voss", "509 Ridge Road Saint Albans, NY 11412", "613-555-0164"),
+(12, "Ashlyn Frenette", "993 Maple Street Sarasota, FL 34231", "613-555-0112"),
+(13, "Rosena Ebeling", "833 Ridge Avenue Concord, NH 03301", "613-555-0141"),
+(14, "Lavina Ploss", "300 8th Avenue Grand Forks, ND 58201", "613-555-0191"),
+(15, "Victor Lopez", "264 Valley Drive Fairfax, VA 22030", "613-555-0187");
+
+INSERT INTO Manufacturer
+VALUES (1, "Go Manufacturer", "805 Holly Drive Palm City, FL 34990", "Franklyn Farnes", "613-555-0131"),
+(2, "Manufacturer Group", "173 Jefferson Street Blacksburg, VA 24060", "Whitney Akey", "613-555-0137"),
+(3, "Manufacturer Media", "375 5th Street South San Carlos, CA 94070", "Elizabeth Aldana", "613-555-0123"),
+(4, "Manufacturer Design", "823 Church Road Quakertown, PA 18951", "Deane Kehl", "613-555-0107"),
+(5, "Pro Manufacturer", "876 Tanglewood Drive Morganton, NC 28655", "Dwain Lathem", "613-555-0180"),
+(6, "Manufacturer Site", "192 Olive Street North Haven, CT 06473", "Denisse Oh", "613-555-0161"),
+(7, "Manufacturer TV", "747 Bank Street Satellite Beach, FL 32937", "Arcelia Cesare", "613-555-0134"),
+(8, "Manufacturer Now", "477 Route 27 Kennesaw, GA 30144", "Elza Beall", "613-555-0152"),
+(9, "Manufacturer Plus", "24 Hamilton Road Parsippany, NJ 07054", "Alyssa Henningsen", "613-555-0108"),
+(10, "All Manufacturer", "913 Colonial Avenue Huntington Station, NY 11746", "Jonathon Quist", "613-555-0187"),
+(11, "Manufacturer Pro", "99 Forest Drive Glenside, PA 19038", "Nu Juarez", "613-555-0162"),
+(12, "Manufacturer Club", "664 Summit Street Camas, WA 98607", "Kimbra Mire", "613-555-0114"),
+(13, "Manufacturer Market", "848 Cambridge Road Deltona, FL 32725", "Robert Bigger", "613-555-0150"),
+(14, "Manufacturer Tech", "243 Brown Street Port Chester, NY 10573", "Stan Kilday", "613-555-0159"),
+(15, "Free Manufacturer", "503 Holly Court West Bend, WI 53095", "Racquel Strozier", "613-555-0178");
