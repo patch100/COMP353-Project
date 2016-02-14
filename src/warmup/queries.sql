b339c95028151be561c2ee5bf4078428c523807c
Gabriel Hardy <gabrielhardy@protonmail.ch>
2016-02-14 11:02:13 -0500

    Did first 3 queries; Added data to match query requirements

--- src/warmup/queries.sql
+++ src/warmup/queries.sql
@@ -7,7 +7,18 @@ WHERE c.cNumber = s.cNumber AND
 	od.pNumber = p.pNumber AND
 	p.pName LIKE '%ware%';
 # Query 2
+SELECT *
+FROM Product p, OrderDetail od
+WHERE od.pNumber = p.pNumber AND
+od.oNumber = 3;
 # Query 3
+SELECT c.cName
+FROM Customer c
+WHERE (SELECT COUNT(od.pNumber)
+		FROM OrderDetail od, Shippment s
+		WHERE od.oNumber = s.oNumber AND
+			s.cNumber = c.cNumber
+	) = 3;
 # Query 4
 # Query 5
 # Query 6
