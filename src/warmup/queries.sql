b0b516b38b5684d10071b852b399db28074e7ca0
Jonny <conundrumjwb@gmail.com>
2016-02-14 18:27:44 -0500

    query 4 done, 5 inc, 6 inc, Inserts done to test 5, 6.

--- src/warmup/queries.sql
+++ src/warmup/queries.sql
@@ -20,6 +20,27 @@ WHERE (SELECT COUNT(od.pNumber)
 			s.cNumber = c.cNumber
 	) = 3;
 # Query 4
+SELECT p.pName, m.mName
+FROM Product p, Manufacturer m
+WHERE p.mNumber = m.mNumber AND p.pUnitprice > 100;  
 # Query 5
+
+SELECT DISTINCT c.cName
+FROM Customer c, Shippment s, OrderDetail od
+WHERE c.cNumber = s.cNumber AND
+    s.oNumber = od.oNumber AND 
+    od.pNumber NOT IN (SELECT p.pNumber
+        FROM Product p, Customer c, Shippment s, OrderDetail od
+        WHERE c.cName LIKE 'Frank%' AND
+            c.cNumber = s.cNumber AND
+            s.oNumber = od.oNumber AND
+            p.pNumber = od.pNumber
+        );
 # Query 6
+SELECT c.cName
+From Customer c
+WHERE (SELECT COUNT(od.pNumber)
+		FROM Customer c, Shippment s, OrderDetail od
+		WHERE c.cNumber = s.cNumber AND
+				s.oNumber = od.oNumber ) = 43;
 # Query 7
