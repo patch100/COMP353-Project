72ff8639f1384e208b470ed08e6940c570880f1b
patch100 <patch.93@gmail.com>
2016-04-13 15:19:59 -0400

    Fixed query 2

--- src/classes/ProductQueryEntity.php
+++ src/classes/ProductQueryEntity.php
@@ -4,8 +4,7 @@ class ProductQueryEntity
     // columns: name, orders, quantity
     protected $name;
     protected $orders;
-    protected $quantity;
-    protected $id;
+    protected $units;
 
     /**
      * Accept an array of data matching properties of this class
@@ -14,25 +13,9 @@ class ProductQueryEntity
      * @param array $data The data to use to create
      */
     public function __construct(array $data) {
-        //TODO UNCOMMENT AND SET PROPER DATA
-        // if(isset($data['id'])) {
-        //     $this->id = $data['id'];
-        // }
-        // $this->name = $data['name'];
-        // $this->address = $data['address'];
-        // $this->phone = $data['phone'];
-        #$this->name = "Fire Truck";
-        #$this->orders = 10;
-        #$this->quantity = 100;
-
-        #if(isset($data['name'])) { $this->name = $data['name']; }
-        if(isset($data['id'])) { $this->id = $data['id']; }
-        if(isset($data['orders'])) { $this->orders = $data['orders']; }
-        if(isset($data['quantity'])) { $this->quantity = $data['quantity']; }
-    }
-
-    public function getId() {
-        return $this->id;
+        if(isset($data['Name'])) { $this->name = $data['Name']; }
+        if(isset($data['UnitsSold'])) { $this->units = $data['UnitsSold']; }
+        if(isset($data['NumberOfOrders'])) { $this->orders = $data['NumberOfOrders']; }
     }
 
     public function getName() {
@@ -43,23 +26,7 @@ class ProductQueryEntity
         return $this->orders;
     }
 
-    public function getQuantity() {
-        return $this->quantity;
-    }
-
-    public function setId($id) {
-        $this->id = $id;
-    }
-
-    public function setName($name) {
-        $this->name = $name;
-    }
-
-    public function setOrders($orders) {
-         $this->orders = $orders;
-    }
-
-    public function setQuantity($quantity) {
-         $this->quantity = $quantity;
+    public function getUnits() {
+        return $this->units;
     }
 }
--- src/classes/ProductQueryMapper.php
+++ src/classes/ProductQueryMapper.php
@@ -1,101 +1,54 @@
 <?php
 class ProductQueryMapper extends Mapper
 {    
-    /**
-     * Gets a list of products
-     *
-     * @return [ProductQuery]  List of products
-     */
-    public function getProducts() {
-        //TODO WRITE SQL (Preferabbly in another file, full of queries)
-        //$sql = "";
-        //$stmt = $this->db->query($sql);
-        //$results = [];
-        //while($row = $stmt->fetch()) {
-        //    $results[] = new CustomerEntity($row);
-        //}
-        $dummy = [];
-        $results[] = new ProductQueryEntity($dummy);
-
-        return $results;
-    }
-
     public function processQuery() {
-        $get_top3_products_sql = "SELECT io.ItemId as id, SUM(io.Quantity) as quantity, COUNT(io.OrderId) as orders
-            FROM ItemsOrdered io
-            WHERE io.OrderId IN (
-                SELECT o.Id
-                FROM Orders o
-                WHERE o.DateOfPurchase < DATE_ADD(NOW(), INTERVAL -12 MONTH))
-            GROUP BY io.ItemId";
+        $sql = "SELECT Item.Name, SUM(ItemsOrdered.Quantity) AS UnitsSold, COUNT(Item.Name) AS NumberOfOrders
+From Item join ItemsOrdered on Item.Id = ItemsOrdered.ItemId
+join  Orders on ItemsOrdered.OrderId = Orders.Id
+join Inventory on ItemsOrdered.ItemId = Inventory.ItemId
+WHERE Orders.DateOfPurchase > date_sub(now(), interval 12 month)
+GROUP BY Item.Name
+ORDER BY SUM(ItemsOrdered.Quantity)*Inventory.Price DESC
+limit 3";
 
-        $stmt = $this->db->query($get_top3_products_sql);
+        $stmt = $this->db->query($sql);
         $results = [];
-
         while($row = $stmt->fetch()) {
             $results[] = new ProductQueryEntity($row);
         }
-
-        foreach ($results as $value) {
-            $item_id = $value->getId();
-
-            $get_item_name_sql = "SELECT Name
-                FROM Item
-                WHERE Id = $item_id";
-
-            $stmt = $this->db->query($get_item_name_sql);
-            $row = $stmt->fetch();
-
-            if ($row)
-                $value->setName($row['Name']);
-            else
-                throw new Exception("Item ID $item_id does not exists in item table");
-        }
-
-        $top3 = [];
-
-        $results_copy = array();
-        $results_copy = $results;
-
-        for($i = 0; $i < 3; $i++) { # Dumbest implementation of a sort ever.
-            $highest_valued_item = $this->getHighestValueItem($results_copy); # Mutative call on results_copy
-            if (!in_array($highest_valued_item, $top3))
-                array_push($top3, $highest_valued_item);
-        }  
-
-        return $top3;
+        return $results;
     }
 
-    private function getHighestValueItem(&$items) {
-        $highest_value = 0;
-        $highest_value_item = NULL;
-        $index = 0;
+    // private function getHighestValueItem(&$items) {
+    //     $highest_value = 0;
+    //     $highest_value_item = NULL;
+    //     $index = 0;
 
-        foreach ($items as $item) {
-            $item_id = $item->getId();
+    //     foreach ($items as $item) {
+    //         $item_id = $item->getId();
 
-            $get_price_of_item = "SELECT Price
-                FROM Inventory
-                WHERE ItemId = $item_id";
+    //         $get_price_of_item = "SELECT Price
+    //             FROM Inventory
+    //             WHERE ItemId = $item_id";
 
-            $stmt = $this->db->query($get_price_of_item);
-            $row = $stmt->fetch();
+    //         $stmt = $this->db->query($get_price_of_item);
+    //         $row = $stmt->fetch();
 
-            if ($row) {
-                $item_price = $row['Price'];
+    //         if ($row) {
+    //             $item_price = $row['Price'];
 
-                $value = $item_price * $item->getQuantity();
+    //             $value = $item_price * $item->getQuantity();
 
-                if ($value > $highest_value) {
-                    $highest_value = $value;
-                    $highest_value_item = $item;
-                    unset($items[$index]);
-                }
-            }
-            else
-                throw new Exception("Item ID $item_id does not exists in inventory table");
-            $index += 1;
-        }
-        return $highest_value_item;
-    }
+    //             if ($value > $highest_value) {
+    //                 $highest_value = $value;
+    //                 $highest_value_item = $item;
+    //                 unset($items[$index]);
+    //             }
+    //         }
+    //         else
+    //             throw new Exception("Item ID $item_id does not exists in inventory table");
+    //         $index += 1;
+    //     }
+    //     return $highest_value_item;
+    // }
 }
